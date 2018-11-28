09922b74a44598c1a7f82627b5e0d5384bd00543
americast <sayan.sinha@iitkgp.ac.in>
2018-11-28 20:16:07 +0530

    zero

---
+++ test/zero/bloc.py
@@ -0,0 +1,34 @@
+import json
+
+f = open("count_bloc.json","r")
+input_json = json.load(f)
+f.close()
+
+def function(elem):
+	return elem[1]
+
+k = 3 # To be input 
+result_dict ={}
+result_list = []
+for each in input_json:
+	count = input_json[each]
+	# print("each", each)
+	each_here = json.loads(each)
+	weights = []
+	total_weight = 0.0
+	for i in range(k):
+		total_weight+=each_here[i][1]
+	for i in range(k):
+		weights.append(k * each_here[i][1]/total_weight)
+	for i in range(k):
+		if each_here[i][0] in result_dict:
+			result_dict[each_here[i][0]]+=(count * weights[i])
+		else:
+			result_dict[each_here[i][0]]=(count * weights[i])
+
+
+for each in result_dict:
+	result_list.append((each,result_dict[each]))
+
+result_list.sort(key=function,reverse=True)
+print(result_list)
